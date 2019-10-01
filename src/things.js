9c9fc9f4a90a45b4cd994fe2203590a5ca9d5ce1
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2019-10-01 09:03:07 -0500

    initial push, added list of needed items to things.js

---
+++ src/things.js
@@ -0,0 +1,25 @@
+// buttons
+//// new
+//// filter
+
+// List
+//// title
+//// stars
+
+// List detailed
+//// title
+//// trashcan
+//
+//// visit button
+//// rating
+//
+//// lorem text
+
+// New bookmark
+//// title input
+//// name input
+//// rating
+//// description input
+//
+//// cancel
+//// create
