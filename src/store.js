ed6d2713e57fe97e2a4ba16febc00e724c394e21
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2019-10-01 20:34:46 -0500

    laying out basics of store.js again

---
+++ src/store.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+import * as api from './api.js';
+
+let currentDb = api.getItems();
+console.log(currentDb);
+
+export const bookmarks = [];
+export const adding = false;
+export const error = null;
+export const filter = 0;
+
+const findById = () => { };
+
+export const handleGet = () => { };
+
+export const handleNew = () => { };
+
+export const handleEdit = () => { };
+
+export const handleDelete = () => { };
+
+export const handleToggle = () => { };
+
+export const handleError = () => { };
+
+export const handleFilter = () => { };
