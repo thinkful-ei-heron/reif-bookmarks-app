65f5de6e464b69ff7e10ec1975a521f360738df0
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2019-10-02 11:53:27 -0500

    starting over

--- src/main.js
+++ src/main.js
@@ -1,6 +1,95 @@
-import * as store from './store.js';
-// import * as render from './render.js';
-// import * as api from './api.js';
+/* eslint-disable no-console */
 
-store.handleNew();
-store.handleFilter();
+// API stuff
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/reif/bookmarks';
+
+const apiFetch = (url, method, body) => {
+  let error = false;
+  return fetch(url, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+    .then(res => {
+      if (!res.ok) {
+        alert(res.status);
+      }
+      return res.json();
+    })
+    .then(data => {
+      if (error) {
+        alert(error);
+        return Promise.reject(error);
+      }
+      return data;
+    });
+};
+
+const getItems = () => {
+  // GET
+  console.log('getting');
+  return apiFetch(`${BASE_URL}`, 'GET');
+};
+
+const createItem = (body) => {
+  // POST
+  console.log('posting');
+  return apiFetch(`${BASE_URL}`, 'POST', body);
+};
+
+const updateItem = (id, body) => {
+  // PATCH
+  console.log('patching');
+  return apiFetch(`${BASE_URL}/${id}`, 'PATCH', body);
+};
+
+const deleteItem = (id) => {
+  // DELETE
+  console.log('deleting');
+  return apiFetch(`${BASE_URL}/${id}`, 'DELETE');
+};
+// End API stuff
+
+// store stuff
+
+const bookmarks = [];
+const adding = false;
+const error = null;
+const filter = 0;
+
+const addBookmark = function (newBookmark) {
+  this.bookmarks.push(newBookmark);
+};
+
+const findBookmark = function (id) {
+  return bookmarks.find(currentBookmark => currentBookmark.id === id);
+};
+
+const deleteBookmark = function (id) {
+  this.bookmarks = this.bookmarks.filter(currentBookmark => currentBookmark.id !== id);
+};
+
+const updateBookmark = function (id, newData) {
+  let currentBookmark = this.findById(id);
+  Object.assign(currentBookmark, newData);
+};
+
+// fix
+// const toggleExpand = function (id) {};
+
+// end store stuff
+
+
+const populateStore = function () {
+  getItems()
+    .then((item) => {
+      item.forEach((item) => {
+        bookmarks.push(item);
+      });
+    });
+};
+
+populateStore();
+console.log(bookmarks);
